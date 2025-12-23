26a913eb89f9fbf8689d4869252f72729f14ced1
Abhishek <boseabhishek2@gmail.com>
2025-12-23 19:33:10 +0100

    [account service] refactoring

--- account_service/src/handlers/account.rs
+++ account_service/src/handlers/account.rs
@@ -14,9 +14,6 @@ use crate::database::{
 };
 
 pub async fn create_account(client: &Client, account_request: AccountRequest) -> (bool, Option<DataKind>, Option<String>) {
-    let mut success = false;
-    let mut data = None;
-    let mut error_message = None;
     let result_customer = get_customer(
         &client, 
         account_request.pan_id, 
@@ -26,43 +23,39 @@ pub async fn create_account(client: &Client, account_request: AccountRequest) ->
 
     if let Err(e) = result_customer {
         eprintln!("Error: {e}");
-        return (success, data, Some("Error: Invalid credentials".to_string()));
+        return (false, None, Some("Error: Invalid credentials".to_string()));
     }
 
     let customer = result_customer.unwrap();
     match add_account(client, customer.id).await {
         Ok(account) => {
-            success = true;
-            let account_api = AccountResponse {
-                account_number: account.account_number,
-                balance: account.balance,
-                creation_timestamp: account.creation_timestamp,
-            };
-            data = Some(DataKind::CreateAccountResponse { account: account_api.clone() });
+            let data = Some(DataKind::CreateAccountResponse { 
+                account: AccountResponse {
+                    account_number: account.account_number,
+                    balance: account.balance,
+                    creation_timestamp: account.creation_timestamp,
+                }
+            });
+            (true, data, None)
         },
         Err(e) => {
             eprintln!("Error: {e}");
-            error_message = Some("Error: Failed to create account".to_string());
+            (false, None, Some("Error: Failed to create account".to_string()))
         }
     }
-    (success, data, error_message)
 }
 
 pub async fn get_account(client: &Client, account_number: String) -> (bool, Option<DataKind>, Option<String>) {
-    let mut success = false;
-    let mut error_message = None;
-    let mut data = None;
     match get_account_db(client, account_number).await {
         Ok(account) => {
-            success = true;
-            data = Some(DataKind::GetAccountResponse { account: account.clone() })
+            let data = Some(DataKind::GetAccountResponse { account: account.clone() });
+            (true, data, None)
         },
         Err(e) => {
             eprintln!("Error: {e}");
-            error_message = Some("Error: Failed to get account".to_string())
+            (false, None, Some("Error: Failed to get account".to_string()))
         }
     }
-    (success, data, error_message)
 }
 
 pub async fn update_balance(client: &Client, account_number: String, balance: f64) -> (bool, Option<DataKind>, Option<String>) {
--- account_service/src/handlers/customer.rs
+++ account_service/src/handlers/customer.rs
@@ -5,29 +5,26 @@ use object::interfaces::{customer::{CustomerResponse, CustomerRequest}, io::Data
 use crate::database::customer::add_customer;
 
 pub async fn create_customer(client: &Client, customer_request: CustomerRequest) -> (bool, Option<DataKind>, Option<String>) {
-    let mut success = false;
-    let mut data = None;
-    let mut error_message = None;
     match add_customer(&client, customer_request).await {
         Ok(customer) => {
-            success = true;
-            let customer_api = CustomerResponse {
-                customer_reference_id: customer.customer_reference_id,
-                first_name: customer.first_name,
-                last_name: customer.last_name,
-                pan_id: customer.pan_id,
-                email_id: customer.email_id,
-                age: customer.age,
-                date_of_birth: customer.date_of_birth,
-                contact_number: customer.contact_number,
-                creation_timestamp: customer.creation_timestamp,
-            };
-            data = Some(DataKind::CreateCustomerResponse { customer: customer_api.clone() })
+            let data = Some(DataKind::CreateCustomerResponse { 
+                customer: CustomerResponse {
+                    customer_reference_id: customer.customer_reference_id,
+                    first_name: customer.first_name,
+                    last_name: customer.last_name,
+                    pan_id: customer.pan_id,
+                    email_id: customer.email_id,
+                    age: customer.age,
+                    date_of_birth: customer.date_of_birth,
+                    contact_number: customer.contact_number,
+                    creation_timestamp: customer.creation_timestamp,
+                }
+            });
+            (true, data, None)
         },
         Err(e) => {
             eprintln!("Error: {e}");
-            error_message = Some("Error: Failed to add customer".to_string());
+            (false, None, Some("Error: Failed to add customer".to_string()))
         },
     }
-    (success, data, error_message)
 }
